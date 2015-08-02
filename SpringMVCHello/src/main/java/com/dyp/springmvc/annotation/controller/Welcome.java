10d8addf297939b6a887e09a3883f61b2b68f9b2
YanpingDong <dyp@tcl.com>
2015-08-02 21:58:35 +0800

    fix bug in Welcome.java.  add a maping that map to all url

--- SpringMVCHello/src/main/java/com/dyp/springmvc/annotation/controller/Welcome.java
+++ SpringMVCHello/src/main/java/com/dyp/springmvc/annotation/controller/Welcome.java
@@ -5,17 +5,23 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Welcome {
 	
-	@RequestMapping("/welcome")
+	@RequestMapping("/**")
+	public void map2All()
+	{
+		System.out.println("match all request URL");
+	}
+	
+	
+	@RequestMapping("welcome")
 	public String welcome()
 	{
 		System.out.println("in annotation controller");
-		HttpHeaders headers = new HttpHeaders();
-	    headers.add("Content-Type", "application/json");
-	    return "welcome";
+	    return "welcome";//return welcome.jsp to client
 	}
 	
 	@RequestMapping("/404/error")
