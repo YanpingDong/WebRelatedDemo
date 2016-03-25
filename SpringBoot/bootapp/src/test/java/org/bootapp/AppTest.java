1d137b0847650e66c4a37b642fb30022ecb90784
YanpingDong <lear521@163.com>
2016-03-25 11:41:13 +0800

    add spring boot profile feature and maven profile feature

--- SpringBoot/bootapp/src/test/java/org/bootapp/AppTest.java
+++ SpringBoot/bootapp/src/test/java/org/bootapp/AppTest.java
@@ -1,38 +1,37 @@
 package org.bootapp;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/**
- * Unit test for simple App.
- */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoApplication.class)
+@WebIntegrationTest("server.port:0")
+@ActiveProfiles("prd")  
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+//	@Value("${for.test.value}")
+//    private String forTestValue;
+	
+	@Value("${spring.application.name}")
+	private String appName;
+	@Value("${spring.profiles.active}")
+	private String springActive;
+	
+	@Value("${logging.config}")
+	private String loggingConfig;
+	
+	@Test
+	public void testSpringBootProfile()
+	{
+		//System.out.println(forTestValue);
+		System.out.println(springActive);
+		System.out.println(loggingConfig);
+		System.out.println(appName);
+	}
+  
 }
